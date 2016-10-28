5eed49c2cb8bbf74cb5687589f4845f62d39fdb3
annave123 <tony.nave@aol.com>
2016-10-27 23:37:53 -1000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        String names[] = new String[100];
+        int grades[] = new int[100];
+        int sum= 0;
+        System.out.println("Please enter the grades that you'd like to use\nWhen completed, please type (done)");
+        Scanner listNames = new Scanner(System.in);
+        for (int i = 0; i < grades.length; i++) {
+            grades[i] = listNames.nextInt();
+            if (grades[1] == -1) {
+                break;
+            }
+            for (int a = 0; i < grades.length; a++) {
+                names[i] = listNames.next();
+
+            }
+            System.out.println();
+            sum = grades[i]/grades.length;
+        }
+    }
+}
